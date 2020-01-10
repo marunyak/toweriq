e7350d56cd5dd383d2a15a7ad33e7ba34fa1f12a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-10 12:59:16 +0000

    Deploy marunyak/toweriq to github.com/marunyak/toweriq.git:gh-pages

---
+++ precache-manifest.3e49f5b0bd85d48f40fe84b2f08d14c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae887034820b190aecfc96d3729615e8",
+    "url": "/toweriq/index.html"
+  },
+  {
+    "revision": "609ba020eb82a0dd9dce",
+    "url": "/toweriq/static/css/main.e342355b.chunk.css"
+  },
+  {
+    "revision": "7ee20aac7fe7a3a21903",
+    "url": "/toweriq/static/js/2.ff7949f8.chunk.js"
+  },
+  {
+    "revision": "609ba020eb82a0dd9dce",
+    "url": "/toweriq/static/js/main.02a3debc.chunk.js"
+  },
+  {
+    "revision": "4d1555d33c0b131616d0",
+    "url": "/toweriq/static/js/runtime-main.6367080d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/toweriq/precache-manifest.3e49f5b0bd85d48f40fe84b2f08d14c6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/toweriq/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
